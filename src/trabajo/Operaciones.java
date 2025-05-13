de15e3ce92ee0441952d56138a404ec6537555dc
AlejoSebas02 <alejobsc0902@gmail.com>
2025-05-13 15:08:54 -0500

    Operaciones AlejoRivera2

--- src/trabajo/Operaciones.java
+++ src/trabajo/Operaciones.java
@@ -9,5 +9,25 @@ package trabajo;
  * @author Sebs
  */
 public class Operaciones {
-    
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    // Agregar multiplicar
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    // Agregar dividir
+    public double dividir(int a, int b) {
+        if (b == 0) {
+            System.out.println("No se puede dividir por cero");
+            return 0;
+        }
+        return (double) a / b;
+    }
 }
