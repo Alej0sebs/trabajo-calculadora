7a803a8f7ef2d2a9e8d827a94664798cbb3589b9
AlejoSebas02 <alejobsc0902@gmail.com>
2025-05-13 15:08:35 -0500

    Cmabios de AlejoRivera2

--- src/trabajo/Trabajo.java
+++ src/trabajo/Trabajo.java
@@ -6,7 +6,15 @@ public class Trabajo {
 
     public static void main(String[] args) {
       
-        
+        Operaciones basicas = new Operaciones();
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Suma: " + basicas.sumar(a, b));
+        System.out.println("Resta: " + basicas.restar(a, b));
+        System.out.println("Multiplicación: " + basicas.multiplicar(a, b));
+        System.out.println("División: " + basicas.dividir(a, b));
     }
     
 }
